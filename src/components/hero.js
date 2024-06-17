a6e781ae49921e4cf25f407534fba28faec0cfc1
Eray <eray.cevik@digitecgalaxus.ch>
2024-06-17 18:17:36 +0200

    started

---
+++ src/components/hero.js
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return(
+    <div>
+        <img className="object-fill h-96" src="Cover10.png"/>
+    </div>
+    )
+}
---
+++ src/components/navigation.js
@@ -0,0 +1,8 @@
+
+export default function Navigation() {
+    return (
+        <div className="h-20 bg-[#040404]">
+            w
+        </div>
+    )
+}
