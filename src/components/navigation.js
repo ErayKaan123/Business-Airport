6cca95278473f9fc3b1c57084c9f9f66b6afc399
Eray <eray.cevik@digitecgalaxus.ch>
2024-06-20 11:01:49 +0200

    s

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,9 +5,9 @@ const lobster = Lobster({ subsets: ["latin"], weight:"400" });
 export default function Navigation() {
     return (
         <div className="fixed flex w-full h-[100px] bg-[#040404] text-[#fffff2]" style={{ flexDirection: "row", alignItems: "center", zIndex: "100",  boxShadow: "0 5px 25px #040404" }}>
-            <nav className="max-w-[100rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
-                <a className="flex-none text-xl font-semibold dark:text-white" href="#">
-                    <img style={{ height: "70%", display: "inline", marginRight: "10px" }} src="Images/RPortIcon.svg" alt="RPort Icon" />
+            <nav className="max-w-[105rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+                <a className="flex-none text-xl mt-8 font-semibold dark:text-white" href="#">
+                    <img style={{ height: "80%", display: "inline", marginRight: "10px" }} src="Images/RPortIcon.svg" alt="RPort Icon" />
                     <p style={{ fontSize: "70px", display: "inline", fontFamily: "Lobster, cursive", fontWeight: "400", textShadow:"0 0 25px #fffff2" }}>RPort</p>
                 </a>
                 <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
