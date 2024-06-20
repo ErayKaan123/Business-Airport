6ce31862069220a862a7205784edddcb601fa7ff
Jean-Luc Jordi <jean-luc.jordi@digitecgalaxus.ch>
2024-06-20 10:50:32 +0200

    design

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,8 +4,19 @@ const lobster = Lobster({ subsets: ["latin"], weight:"400" });
 
 export default function Navigation() {
     return (
-        <div style={{position: "fixed", width: "100%", height: "100px", backgroundColor: "#040404", color:"#fffff2", display: "flex", flexDirection:"row", alignItems:"center", fontSize:"70px", zIndex: "100"}}>
-            <img style={{height: "70%", display:"inline", marginRight:"10px" }} src="Images/RPortIcon.svg"/><p style={{fontSize: "100%", display:"inline", fontFamily: lobster.style.fontFamily, fontWeight:"400" }}>RPort</p>
+        <div style={{
+            position: "fixed", 
+            width: "100%", 
+            height: "100px", 
+            backgroundColor: "#040404", 
+            color:"#fffff2", 
+            display: "flex", 
+            flexDirection:"row", 
+            alignItems:"center", 
+            fontSize:"70px", 
+            zIndex: "100",
+            boxShadow: "0 5px 25px #040404"}}>
+            <img style={{height: "70%", display:"inline", marginRight:"10px",  }} src="Images/RPortIcon.svg"/><p style={{fontSize: "100%", display:"inline", fontFamily: lobster.style.fontFamily, fontWeight:"400", textShadow:"0 0 25px #fffff2" }}>RPort</p>
         </div>
     )
 }
