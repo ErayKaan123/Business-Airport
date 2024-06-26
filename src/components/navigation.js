85a571ace44a73e173e127e15a327a0a4ab0354c
Jean-Luc Jordi <jean.jordi@lernende.bbw.ch>
2024-06-26 09:42:05 +0200

    added navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -12,15 +12,19 @@ export default function Navigation() {
             color:"#fffff2", 
             display: "flex", 
             flexDirection:"row", 
-            alignItems:"center", 
+            alignItems:"center",
+            justifyContent: "space-between",
             fontSize:"70px", 
             zIndex: "100",
             boxShadow: "0 5px 25px #040404"}}>
-            <img style={{height: "70%", display:"inline", marginRight:"10px",  }} src="Images/RPortIcon.svg"/><p style={{fontSize: "100%", display:"inline", fontFamily: lobster.style.fontFamily, fontWeight:"400", textShadow:"0 0 25px #fffff2" }}>RPort</p>
-            <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
-                    <a className="font-medium text-lg text-white" href="#" aria-current="page">Home</a>
+                <div>
+            <img style={{height: "70%", display:"inline", marginRight:"10px",  }} src="Images/RPortIcon.svg"/>
+            <p style={{fontSize: "100%", display:"inline", fontFamily: lobster.style.fontFamily, fontWeight:"400", textShadow:"0 0 25px #fffff2" }}>RPort</p>
+            </div>
+            <div className="flex flex-row items-center gap-5 mt-5 mr-5 sm:justify-end sm:mt-0 sm:ps-5">
+                    <a className="font-medium text-lg text-white" href="/" aria-current="page">Home</a>
                     <a className="font-medium text-lg text-neutral-400 hover:text-white" href="#">Buy Tickets</a>
-                    <a className="font-medium text-lg text-neutral-400 hover:text-white " href="#">Calendar</a>
+                    <a className="font-medium text-lg text-neutral-400 hover:text-white " href="/Calendar">Calendar</a>
                     <a className="font-medium text-lg text-neutral-400 hover:text-white" href="#">Contact</a>
             </div>
         </div>
