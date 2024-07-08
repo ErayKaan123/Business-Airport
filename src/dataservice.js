def7e7454d95300350b534afe3aca378d76ab5c3
Eray <eray.cevik@digitecgalaxus.ch>
2024-07-08 15:14:22 +0200

    working

--- src/dataservice.js
+++ src/dataservice.js
@@ -1,17 +1,25 @@
-export async function getCalendarEntriesByUserId(id) {
-    const response = await fetch("http://localhost:3001/calendarEntries?userId=" + id);
+export async function getCalendarEntriesByUserId(userId) {
+    const response = await fetch("http://localhost:3001/calendarEntries?userId=" + userId);
     if(response.ok)return response.json();
     throw response;
 }
 
-export async function addEntryToUserById(id, entry) {
+export async function addEntryToUserById(userId, entry) {
     const response = await fetch("http://localhost:3001/calendarEntries?userId=" + id);
     console.log(response.json().then((response) => {return response}).result)
-    fetch('http://localhost:3001/calendarEntries?userId=' + id,{
+    fetch('http://localhost:3001/calendarEntries?userId=' + userId,{
         method:'POST',
         headers:{
             Accept:'application/json','Content-Type':'application/json',},
             body: JSON.stringify(entry),
         });
+}
+
+export async function deleteCalendarEntryById(userId, calendarId) {
+    console.log(userId);
+}
+
+export async function deleteAllCalendarEntries(userId,) {
+    
 }
 /*+ "products?category=" + category*/
